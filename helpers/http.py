e7cedb2bb9ab87a49b5efa280ab14cec40416d20
Johan Gonzalez <jopi20101@gmail.com>
2022-03-14 12:42:08 -0600

    Done: Chat socket client added

--- helpers/http.py
+++ helpers/http.py
@@ -1,4 +1,6 @@
+from traceback import print_tb
 import requests
 def do_call(url,method=''):
     if(method.upper()=='GET'):
         requests.get(url)
+        print("The attack has been successful")
--- main.py
+++ main.py
@@ -8,21 +8,21 @@ def main():
   net_exec = network_commads.Command_executer(target_ip)
   vul_exec= vulnerability.Vulnerability_executor(target_ip)
   crip_exec = criptography.Criptography_executor("HOLA MUNDO")
-  target_netcommnads_funcs = [net_exec.tracert_ip,net_exec.ping_ip,net_exec.arp]
+  target_netcommnads_funcs = [net_exec.tracert_ip,net_exec.ping_ip,net_exec.arp, net_exec.lsof]
   target_vulnerability_funcs = [vul_exec.exec_ddos_attack]
   target_criptography_funcs = [crip_exec.exec_cipher,crip_exec.generate_key]
 
   # Run networkin commands threads
-  # for i in range(len(target_netcommnads_funcs)):
-  #   th = threading.Thread(target=target_netcommnads_funcs[i])
-  #   th.start()
-  #   th.join()
+  for i in range(len(target_netcommnads_funcs)):
+    th = threading.Thread(target=target_netcommnads_funcs[i])
+    th.start()
+    th.join()
 
   # Run vulnerability funcs
-  # for func in target_vulnerability_funcs:
-  #   th = threading.Thread(target=func)
-  #   th.start()
-  #   th.join()
+  for func in target_vulnerability_funcs:
+    th = threading.Thread(target=func)
+    th.start()
+    th.join()
 
 # Run criptography funcs
   for func in target_criptography_funcs:
